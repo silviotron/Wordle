62e1ead85458b908c0e289e9b012818705b322e6
“silviotron” <silvionovasmartinez@gmail.com>
2022-05-12 14:02:47 +0200

    ahora el diccionario depende de un documento donde se guarda el Set<String>  diccionario como Objeto y no como texto

--- src/motores/MotorArchivo.java
+++ src/motores/MotorArchivo.java
@@ -4,8 +4,18 @@
  */
 package motores;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,6 +31,8 @@ public class MotorArchivo implements IMotor {
 
     public static String ruta = Path.of(".") + File.separator + "data" + File.separator + "motorAlumnos.dat";
     public static File archivo = new File(ruta);
+    public static String ruta2 = Path.of(".") + File.separator + "data" + File.separator + "diccionario.dat";
+    public static File archivo2 = new File(ruta2);
     private static Set<String> diccionario;
 
     public MotorArchivo() {
@@ -31,7 +43,24 @@ public class MotorArchivo implements IMotor {
             } catch (IOException iOException) {
             }
         }
-        diccionario = GestorArchivos.values(archivo, 5);
+        //diccionario = GestorArchivos.values(archivo, 5);
+        if (!archivo2.exists()) {
+            archivo2.getParentFile().mkdirs();
+            try {
+                archivo2.createNewFile();
+            } catch (IOException iOException) {
+            }
+            try(OutputStream os = new BufferedOutputStream(new FileOutputStream(archivo2));
+                ObjectOutputStream out = new ObjectOutputStream(os)){
+                out.writeObject(GestorArchivos.values(archivo, 5));
+            } catch (Exception e) {}
+        }else{
+            try (InputStream is = new BufferedInputStream(new FileInputStream(archivo2));
+                ObjectInputStream di = new ObjectInputStream(is) ){
+                diccionario = (HashSet<String>)di.readObject();
+            } catch (Exception e) {}
+            
+        }        
 
     }
 
